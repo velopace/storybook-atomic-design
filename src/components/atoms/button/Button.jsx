288ada2b77bb685944e41d39b21ba61a09f9ba8b
velopace <velopace@outlook.com>
2022-11-29 06:06:58 -0600

    default them and styling button

--- src/components/atoms/button/Button.jsx
+++ src/components/atoms/button/Button.jsx
@@ -1,6 +1,30 @@
-const Button = ({ children, href, onClick }) => {
-  if (!href) return <button onClick={onClick}>{children}</button>;
-  return <a href={href}>{children}</a>;
+import { func, node, string } from "prop-types";
+
+import StyledButton, { StyledLinkButton } from "./Button.styles";
+
+const Button = ({ children, href, onClick, variant }) => {
+  if (!href)
+    return (
+      <StyledButton onClick={onClick} variant={variant}>
+        {children}
+      </StyledButton>
+    );
+  return (
+    <StyledLinkButton href={href} variant={variant}>
+      {children}
+    </StyledLinkButton>
+  );
+};
+
+Button.propTypes = {
+  children: node.isRequired,
+  href: string,
+  onClick: func,
+  variant: string,
+};
+
+Button.defaultProps = {
+  variant: "primary",
 };
 
 export default Button;
